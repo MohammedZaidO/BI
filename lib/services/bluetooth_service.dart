f840a6e3304d576e06511ffa6c4a2b8518145a62
Mohammed Zaid <zaid@example.com>
2026-04-05 15:46:35 +0530

    Fix: Final License enum value correction (License.ivd)

--- lib/services/bluetooth_service.dart
+++ lib/services/bluetooth_service.dart
@@ -41,7 +41,7 @@ class BluetoothService {
             await FlutterBluePlus.stopScan();
             
             try {
-              await _device!.connect(timeout: const Duration(seconds: 10), license: const License());
+              await _device!.connect(timeout: const Duration(seconds: 10), license: License.ivd);
               _isConnected = true;
               onConnectionChanged?.call(true);
               
---
+++ tmp_test_enum.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+
+void main() {
+  print('License enum values:');
+  for (var value in License.values) {
+    print('- $value');
+  }
+}
